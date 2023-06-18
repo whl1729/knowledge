5a26511ebb07b33ea6d8e981169ef23c91ea5886
along <wuhl6@mail2.sysu.edu.cn>
2023-06-18 16:27:45 +0800

    add argparse demo

---
+++ computer/python/api/snippets/argparse_demo.py
@@ -0,0 +1,15 @@
+import argparse
+parser = argparse.ArgumentParser()
+# add a positional integer argument
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+# add a optional boolean argument
+parser.add_argument("-v", "--verbose", action="store_true", default=False,
+                    help="increase output verbosity")
+# add a optional integer argument (The default type is "str")
+parser.add_argument("-p", "--port", type=int, default=8080,
+                    help="port number of the server")
+# add a optional string argument
+parser.add_argument("-n", "--name",
+                    help="host name of the server")
+args = parser.parse_args()
